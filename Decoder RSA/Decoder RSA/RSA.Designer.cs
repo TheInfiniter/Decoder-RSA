3db6743ef006e8061d60ad88da17adadfa3428a6
TheInfiniter <50056615+TheInfiniter@users.noreply.github.com>
2019-05-28 16:46:25 +0300

    Add files via upload

---
+++ Decoder RSA/Decoder RSA/RSA.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Decoder_RSA
+{
+    partial class RSA
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RSA));
+            this.SourceText = new System.Windows.Forms.RichTextBox();
+            this.CipherText = new System.Windows.Forms.RichTextBox();
+            this.DecipherText = new System.Windows.Forms.RichTextBox();
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.OpenedKeyText = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.ClosedKeyText = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel4 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialLabel5 = new MaterialSkin.Controls.MaterialLabel();
+            this.CipherFlatButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.DecipherFlatButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.LoadFromFileFlatButton = new MaterialSkin.Controls.MaterialFlatButton();
+            this.LoadFromFile = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // SourceText
+            // 
+            this.SourceText.Location = new System.Drawing.Point(12, 73);
+            this.SourceText.Name = "SourceText";
+            this.SourceText.Size = new System.Drawing.Size(380, 138);
+            this.SourceText.TabIndex = 0;
+            this.SourceText.Text = "Зашифруй меня!";
+            // 
+            // CipherText
+            // 
+            this.CipherText.Location = new System.Drawing.Point(398, 73);
+            this.CipherText.Name = "CipherText";
+            this.CipherText.Size = new System.Drawing.Size(380, 138);
+            this.CipherText.TabIndex = 3;
+            this.CipherText.Text = "";
+            // 
+            // DecipherText
+            // 
+            this.DecipherText.Location = new System.Drawing.Point(12, 269);
+            this.DecipherText.Name = "DecipherText";
+            this.DecipherText.Size = new System.Drawing.Size(380, 138);
+            this.DecipherText.TabIndex = 4;
+            this.DecipherText.Text = "";
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(140, 214);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(125, 19);
+            this.materialLabel1.TabIndex = 12;
+            this.materialLabel1.Text = "Исходный текст";
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(563, 214);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(51, 19);
+            this.materialLabel2.TabIndex = 13;
+            this.materialLabel2.Text = "Шифр";
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(112, 410);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(181, 19);
+            this.materialLabel3.TabIndex = 14;
+            this.materialLabel3.Text = "Расшифрованный текст";
+            // 
+            // OpenedKeyText
+            // 
+            this.OpenedKeyText.Depth = 0;
+            this.OpenedKeyText.Hint = "";
+            this.OpenedKeyText.Location = new System.Drawing.Point(549, 269);
+            this.OpenedKeyText.MouseState = MaterialSkin.MouseState.HOVER;
+            this.OpenedKeyText.Name = "OpenedKeyText";
+            this.OpenedKeyText.PasswordChar = '\0';
+            this.OpenedKeyText.SelectedText = "";
+            this.OpenedKeyText.SelectionLength = 0;
+            this.OpenedKeyText.SelectionStart = 0;
+            this.OpenedKeyText.Size = new System.Drawing.Size(98, 23);
+            this.OpenedKeyText.TabIndex = 15;
+            this.OpenedKeyText.UseSystemPasswordChar = false;
+            // 
+            // ClosedKeyText
+            // 
+            this.ClosedKeyText.Depth = 0;
+            this.ClosedKeyText.Hint = "";
+            this.ClosedKeyText.Location = new System.Drawing.Point(549, 304);
+            this.ClosedKeyText.MouseState = MaterialSkin.MouseState.HOVER;
+            this.ClosedKeyText.Name = "ClosedKeyText";
+            this.ClosedKeyText.PasswordChar = '\0';
+            this.ClosedKeyText.SelectedText = "";
+            this.ClosedKeyText.SelectionLength = 0;
+            this.ClosedKeyText.SelectionStart = 0;
+            this.ClosedKeyText.Size = new System.Drawing.Size(98, 23);
+            this.ClosedKeyText.TabIndex = 16;
+            this.ClosedKeyText.UseSystemPasswordChar = false;
+            // 
+            // materialLabel4
+            // 
+            this.materialLabel4.Depth = 0;
+            this.materialLabel4.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel4.Location = new System.Drawing.Point(411, 269);
+            this.materialLabel4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel4.Name = "materialLabel4";
+            this.materialLabel4.Size = new System.Drawing.Size(132, 23);
+            this.materialLabel4.TabIndex = 17;
+            this.materialLabel4.Text = "Открытый ключ";
+            this.materialLabel4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // materialLabel5
+            // 
+            this.materialLabel5.Depth = 0;
+            this.materialLabel5.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel5.Location = new System.Drawing.Point(411, 304);
+            this.materialLabel5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel5.Name = "materialLabel5";
+            this.materialLabel5.Size = new System.Drawing.Size(132, 23);
+            this.materialLabel5.TabIndex = 18;
+            this.materialLabel5.Text = "Закрытый ключ";
+            this.materialLabel5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // CipherFlatButton
+            // 
+            this.CipherFlatButton.AutoSize = true;
+            this.CipherFlatButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.CipherFlatButton.Depth = 0;
+            this.CipherFlatButton.Location = new System.Drawing.Point(414, 370);
+            this.CipherFlatButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.CipherFlatButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.CipherFlatButton.Name = "CipherFlatButton";
+            this.CipherFlatButton.Primary = false;
+            this.CipherFlatButton.Size = new System.Drawing.Size(116, 36);
+            this.CipherFlatButton.TabIndex = 19;
+            this.CipherFlatButton.Text = "Зашифровать";
+            this.CipherFlatButton.UseVisualStyleBackColor = true;
+            this.CipherFlatButton.Click += new System.EventHandler(this.CipherFlatButton_Click);
+            // 
+            // DecipherFlatButton
+            // 
+            this.DecipherFlatButton.AutoSize = true;
+            this.DecipherFlatButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.DecipherFlatButton.Depth = 0;
+            this.DecipherFlatButton.Location = new System.Drawing.Point(538, 370);
+            this.DecipherFlatButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.DecipherFlatButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.DecipherFlatButton.Name = "DecipherFlatButton";
+            this.DecipherFlatButton.Primary = false;
+            this.DecipherFlatButton.Size = new System.Drawing.Size(126, 36);
+            this.DecipherFlatButton.TabIndex = 20;
+            this.DecipherFlatButton.Text = "Расшифровать";
+            this.DecipherFlatButton.UseVisualStyleBackColor = true;
+            this.DecipherFlatButton.Click += new System.EventHandler(this.DecipherFlatButton_Click);
+            // 
+            // LoadFromFileFlatButton
+            // 
+            this.LoadFromFileFlatButton.AutoSize = true;
+            this.LoadFromFileFlatButton.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.LoadFromFileFlatButton.Depth = 0;
+            this.LoadFromFileFlatButton.Location = new System.Drawing.Point(672, 370);
+            this.LoadFromFileFlatButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.LoadFromFileFlatButton.MouseState = MaterialSkin.MouseState.HOVER;
+            this.LoadFromFileFlatButton.Name = "LoadFromFileFlatButton";
+            this.LoadFromFileFlatButton.Primary = false;
+            this.LoadFromFileFlatButton.Size = new System.Drawing.Size(81, 36);
+            this.LoadFromFileFlatButton.TabIndex = 21;
+            this.LoadFromFileFlatButton.Text = "Загрузка";
+            this.LoadFromFileFlatButton.UseVisualStyleBackColor = true;
+            this.LoadFromFileFlatButton.Click += new System.EventHandler(this.LoadFromFileFlatButton_Click);
+            // 
+            // LoadFromFile
+            // 
+            this.LoadFromFile.FileName = "LoadFromFile";
+            // 
+            // RSA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(790, 443);
+            this.Controls.Add(this.LoadFromFileFlatButton);
+            this.Controls.Add(this.DecipherFlatButton);
+            this.Controls.Add(this.CipherFlatButton);
+            this.Controls.Add(this.materialLabel5);
+            this.Controls.Add(this.materialLabel4);
+            this.Controls.Add(this.ClosedKeyText);
+            this.Controls.Add(this.OpenedKeyText);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialLabel1);
+            this.Controls.Add(this.DecipherText);
+            this.Controls.Add(this.CipherText);
+            this.Controls.Add(this.SourceText);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RSA";
+            this.Text = "Алгоритм RSA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox SourceText;
+        private System.Windows.Forms.RichTextBox CipherText;
+        private System.Windows.Forms.RichTextBox DecipherText;
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private MaterialSkin.Controls.MaterialSingleLineTextField OpenedKeyText;
+        private MaterialSkin.Controls.MaterialSingleLineTextField ClosedKeyText;
+        private MaterialSkin.Controls.MaterialLabel materialLabel4;
+        private MaterialSkin.Controls.MaterialLabel materialLabel5;
+        private MaterialSkin.Controls.MaterialFlatButton CipherFlatButton;
+        private MaterialSkin.Controls.MaterialFlatButton DecipherFlatButton;
+        private MaterialSkin.Controls.MaterialFlatButton LoadFromFileFlatButton;
+        private System.Windows.Forms.OpenFileDialog LoadFromFile;
+    }
+}
+
---
+++ Decoder RSA/Decoder RSA/RSA.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Text;
+using System.Numerics;
+using System.Windows.Forms;
+using System.IO;
+using MaterialSkin;
+using MaterialSkin.Controls;
+
+namespace Decoder_RSA
+{
+    public partial class RSA : MaterialForm
+    {
+        int encryption, decryption, module;
+        string OpenText;
+        byte[] byteArray;
+        int[] cipherArray;
+
+        public RSA()
+        {
+            MaterialSkinManager materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Cyan800, 
+                Primary.Cyan900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+            InitializeComponent();
+        }
+
+        private bool Ferma(long simple) //тест Ферма на простоту
+        {
+            if (simple == 2)
+                return true; //двойка по умолчанию является простым числом
+
+            Random rand = new Random();
+            long test;
+            for (int i = 0; i < 150; i++) //проведем тест 150 раз
+            {
+                
+                test = rand.Next((int)simple); //берем случайное число не больше проверяемого
+
+                if (test <= 2)
+                    test += 2; //избавляемся от ошибки с двойкой
+
+                if (BigInteger.ModPow(test, simple - 1, simple) != 1) //если число не простое, то сразу же прекращаем проверку
+                    return false;
+            }
+            return true;
+        }
+
+        private long SimpleEuclid(long a, long b) //простой алгоритм Евклида (поиск НОД)
+        {
+            //если b = 0, то параметр а будет НОД, если нет, то рекурсивно вызываем метод, пока не получим НОД
+            return ((b > 0) ? SimpleEuclid(b, a % b) : a);
+        }
+
+        private int ExtendedEuclid(int a, int b, out int x, out int y) //расширенный алгоритм Евклида для поиска параметра d
+        {
+            if (a == 0) //если а == 0, то НОД - b, а параметры равны 0 и 1
+            {
+                x = 0;
+                y = 1;
+                return b;
+            }
+
+            //если же а != 0, то рекурсивно вызываем метод, пока не найдем НОД (параметры забираем по ссылке)
+            int x_next, y_next;
+            int d_param = ExtendedEuclid(b % a, a, out x_next, out y_next);
+            x = y_next - (b / a) * x_next;
+            y = x_next;
+
+            return d_param;
+        }
+
+        private void Cipher(out int encode_param, out int decrypt_param, out int n_mod) //шифрование данных
+        {
+            //по условию задачи почти все параметры должны быть уничтожены после успешного шифрования, 
+            //поэтому определяем переменные здесь, а не в глобальной области, чтобы после выхода из функции они удалились
+
+            int p_param, q_param, eyler_func, w_param; //параметры p и q, модуль системы n, функция Эйлера
+            bool check = false; //результат проверки теста Ферма
+            Random rand = new Random();
+
+            do
+            {
+                p_param = rand.Next(32750); //ищем случайное число в диапазоне от нуля до половины int, чтобы не было переполнения
+                check = Ferma(p_param); //и проверяем его на простоту с помощью теста Ферма
+            }
+            while (check == false);
+
+            check = false;
+            do
+            {
+                q_param = rand.Next(32750); //повторяем то же самое для другого числа, в итоге получим два простых числа
+                check = Ferma(q_param);
+            }
+            while (check == false);
+
+            n_mod = p_param * q_param; //вычисляем модуль системы для дальнейшего использования
+            eyler_func = (p_param - 1) * (q_param - 1); //функция Эйлера для подбора параметра шифровки d
+
+            do
+            {
+                encode_param = rand.Next(8000);
+                ExtendedEuclid(encode_param, eyler_func, out w_param, out decrypt_param);
+            }
+            while (SimpleEuclid(eyler_func, encode_param) != 1 || w_param < 0);
+            decrypt_param = w_param;
+        }
+
+        private void LoadFromFileFlatButton_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog read = new OpenFileDialog();
+            read.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            if (read.ShowDialog() == DialogResult.OK)
+            {
+                SourceText.Text = File.ReadAllText(read.FileName, Encoding.Default);
+            }
+        }
+
+        private void CipherFlatButton_Click(object sender, EventArgs e)
+        {
+            CipherText.Text = "";
+            OpenText = SourceText.Text;
+
+            //берем строку с нашим текстом, преобразуем ее в кириллицу, и превращаем в набор байт
+            byteArray = Encoding.GetEncoding(1251).GetBytes(OpenText);
+
+            cipherArray = new int[byteArray.Length]; //в массиве будем хранить все числа шифротекста
+
+            //вызываем функцию шифрования, из нее получаем необходимые параметры
+            Cipher(out encryption, out decryption, out module);
+
+            //кладем ключи в текстовые поля
+            OpenedKeyText.Text = Convert.ToString(encryption);
+            ClosedKeyText.Text = Convert.ToString(decryption);
+
+            //применяем параметры к тексту, получаем шифр
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                cipherArray[i] = (int)BigInteger.ModPow(byteArray[i], encryption, module);
+                CipherText.Text += cipherArray[i];
+                CipherText.Text += " ";
+            }
+        }
+
+        private void DecipherFlatButton_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < byteArray.Length; i++)
+            {
+                byteArray[i] = (byte)BigInteger.ModPow(cipherArray[i], decryption, module);
+            }
+
+            OpenText = Encoding.GetEncoding(1251).GetString(byteArray);
+
+            DecipherText.Text = OpenText;
+        }
+    }
+}
